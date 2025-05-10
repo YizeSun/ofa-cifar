6859984e4dc17e141ff20a9b1ee372b5b3e3b680
Yize_Sun <yize.sun@campus.lmu.de>
2025-05-10 22:42:43 +0200

    updtae test rocm

--- test_rocm.py
+++ test_rocm.py
@@ -1,5 +1,14 @@
 import torch
-print(torch.__version__)
+
+print(torch.version)
+print(torch.version.cuda)
 print(torch.version.hip)
+
+# Expected outputs
+# 2.6
+# None
+# 6.2 # or another ROCm version
+
+print(torch.__version__)
 x = torch.rand(2, 2).to("cuda")
 print(x @ x)
